adfbf3995e54de74178ecd51844141e6e7524b55
ornelly <ornellymauwa2000@gmail.com>
2025-04-24 12:56:32 +0300

    local service added

---
+++ client/src/pages/afterQrcode.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import '../styles/afterqrcode.css';
+
+const AfterQrcode = () => {
+    return (
+        <div>
+
+            <p>You have scanned the code successfully.</p>
+
+        </div>
+    );
+};
+
+export default AfterQrcode;
